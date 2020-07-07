0e9d03bf95586ab2d15b8deae4936e20270d076c
DTMDENIZLI <DTMDENIZLI@DTMDENIZLI>
2020-07-07 16:30:56 +0300

    ef added

--- Startup.cs
+++ Startup.cs
@@ -7,10 +7,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using sampleAPI.Data;
 using sampleAPI.Services.CharacterService;
 
 namespace sampleAPI
@@ -27,6 +29,7 @@ namespace sampleAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(x=>x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddAutoMapper(typeof(Startup));
             services.AddScoped<ICharacterService,CharacterService>();
